edd296c1fcba13f156f390a3db294746c8582021
fredchege19 <fredchege19@gmail.com>
2020-06-14 00:09:13 -1200

    first commit

---
+++ SampleAddOn/AddOnBasePlugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CXS.Retail.Extensibility;
+using System.Windows.Forms;
+
+namespace SampleAddOnForMCEvents
+{
+    /// <summary>
+    /// Add on details like name ,company ,description,version etc.
+    /// </summary>
+    class AddOnBasePlugin:BasePlugin
+    {           
+        public override string CompanyName
+        {
+          get { return "SoftConsult Ltd"; }
+        }   
+        
+        public override string Description
+        {
+            get { return "Block request number if it does not  match the numbering series";}
+        }
+
+        /// <summary>
+        /// Start up method for add on. Here, we can define custom menus for MC or POS.
+        /// </summary>
+        public override void Start()
+        {
+            base.Start();         
+        }
+
+        public override void ShutDown()
+        {
+            base.ShutDown();
+        }
+
+        //Specify addon version.
+        public override Version VersionInfo
+        {
+            get
+            {
+                return new Version("1.0.0.0");
+            }            
+        }
+
+        public override string Name
+        {
+            get { return "AddonForRequestNumber"; }
+        }
+    }
+}
---
+++ SampleAddOn/StockTransferRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CXS.Retail.Extensibility.Modules.Inventory;
+using CXS.SubSystem.Inventory;
+using System.Windows.Forms;
+using CXS.Retail.Extensibility;
+
+namespace SampleAddOnForMCEvents
+{
+    class StockTransferRequest : StockTransferRequestViewModuleBase
+    {
+        /// <summary>
+        /// On Before Cancel on StockTransferRequestAddEditView under Business Transactions .
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+
+        public override void OnBeforeSave(object sender, EventArgs<CXS.SubSystem.Inventory.StockTransferRequest> args)
+        {
+            if(args.Item.RequestNumber != args.Item.Id)
+            {
+                args.Cancel = true;
+                args.Message = "Stock transfer request number must match with request number";
+                args.MessageType = CXS.Retail.ViewModel.Message.MessageType.Error;
+            }
+        }
+
+    }
+}
